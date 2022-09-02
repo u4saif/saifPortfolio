a149abedb24ce0869c19b31f68bdf53fc38fa3b2
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2022-09-02 11:08:40 +0530

    firebase update

---
+++ assets/js/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwWdwfhwrU24M6zYt0b8c8okL3AgL4M6M",
+  authDomain: "besaif.firebaseapp.com",
+  databaseURL: "https://besaif-default-rtdb.firebaseio.com",
+  projectId: "besaif",
+  storageBucket: "besaif.appspot.com",
+  messagingSenderId: "65475740628",
+  appId: "1:65475740628:web:023207aa8d014460188c41",
+  measurementId: "G-NV54K15K49"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
